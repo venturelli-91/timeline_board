500634650aacd05a6449d6f166793291bff2c465
Aurélio Venturelli <venturelli.dev@gmail.com>
2025-09-13 17:33:12 -0300

    feat: Adição de lógica com gerenciamento de estado para visualização da tarefa descrita nos cards usando Tooltips diretamente da documentação oficial do flowbite

---
+++ frontend/app/src/components/ui/ButtonTooltip.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ButtonTooltip = () => {
+	return <div>ButtonTooltip</div>;
+};
+
+export default ButtonTooltip;
--- frontend/app/src/components/ui/CustomCard.tsx
+++ frontend/app/src/components/ui/CustomCard.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 interface CustomCardProps {
 	title?: string;
@@ -6,17 +6,27 @@ interface CustomCardProps {
 }
 
 const CustomCard = ({ title, date }: CustomCardProps) => {
+	const [showTooltip, setShowTooltip] = useState(false);
 	return (
-		<>
-			<a
-				href="#"
-				className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-sm hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-				<h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-					{title}
-				</h5>
-				<p className="font-normal text-gray-700 dark:text-gray-400">{date}</p>
-			</a>
-		</>
+		<div className="relative p-4 bg-white rounded-lg shadow-md">
+			<div
+				className="font-semibold text-lg cursor-pointer inline-block relative"
+				onMouseEnter={() => setShowTooltip(true)}
+				onMouseLeave={() => setShowTooltip(false)}>
+				{title}
+				{showTooltip && (
+					<div
+						role="tooltip"
+						className="absolute left-1/2 -translate-x-1/2 top-full mt-2 px-3 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg shadow-xs opacity-100">
+						{title}
+						<div
+							className="tooltip-arrow"
+							data-popper-arrow></div>
+					</div>
+				)}
+			</div>
+			<div>{date}</div>
+		</div>
 	);
 };
 export default CustomCard;
